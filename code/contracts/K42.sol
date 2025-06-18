2786a59a0de6807035389c277c73cc3b75d65b07
Keny Ramjattan <kramjatt@1A1.42angouleme.fr>
2025-06-18 11:06:27 +0200

    feat: block mint/transfer on pause, parameterize cap/deploy tokens

--- code/contracts/K42.sol
+++ code/contracts/K42.sol
@@ -9,30 +9,33 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract KToken is ERC20, ERC20Burnable, ERC20Capped, Ownable, Pausable {
-    
+contract K42Token is ERC20, ERC20Burnable, ERC20Capped, Ownable, Pausable
+{
+    uint256 internal constant tokensCap = 4200000000;
+    uint256 internal constant deployTokens = tokensCap / 100;
+
     // Constructor sets the token name, symbol, and max supply (cap)
-    constructor(uint256 cap) ERC20("K42Token", "K42") ERC20Capped(cap * 10 ** decimals()) Ownable(msg.sender) {
-        _mint(msg.sender, 42000000 * 10 ** decimals());
+    constructor() ERC20("K42Token", "K42") ERC20Capped(tokensCap * 10 ** decimals()) Ownable(msg.sender) {
+        _mint(msg.sender, deployTokens * 10 ** decimals());
     }
 
-    // Mint new tokens to a given address; only the owner can call this
-    function mint(address to, uint256 amount) external onlyOwner {
+    // Mint new tokens to a given address; Only the owner can call this
+    function mint(address to, uint256 amount) external onlyOwner whenNotPaused {
         _mint(to, amount);
     }
 
-    // Pause all token transfers; only the owner can call this
+    // Pause all token transfers; Only the owner can call this
     function pause() external onlyOwner {
         _pause();
     }
 
-    // Unpause token transfers; only the owner can call this
+    // Unpause token transfers; Only the owner can call this
     function unpause() external onlyOwner {
         _unpause();
     }
 
     // Override required due to multiple inheritance for internal update logic
-    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Capped) {
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Capped) whenNotPaused {
         super._update(from, to, value);
     }
 }
