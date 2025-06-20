55965fc59bd5b57b8b4d7eb8cf870186a9306d00
Rart3x <ramjattankeny971@gmail.com>
2025-06-20 17:13:36 +0200

    fix: Pausable import

--- deployment/contracts/K42.sol
+++ deployment/contracts/K42.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract K42Token is ERC20, ERC20Burnable, ERC20Capped, Pausable, Ownable
